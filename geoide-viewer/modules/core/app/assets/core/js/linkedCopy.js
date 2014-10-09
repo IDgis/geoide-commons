af250ed0b2b40e9728db09a6e745cea18856666b
Erik Orbons <erik.orbons@idgis.nl>
2014-10-09 15:01:54 +0200

    Added LinkedObject class.

--- geoide-viewer/modules/core/app/assets/core/js/linkedCopy.js
+++ geoide-viewer/modules/core/app/assets/core/js/linkedCopy.js
@@ -1,33 +1,43 @@
 define ([
 	'dojo/_base/declare',
+	'dojo/_base/lang',
 	
-	'./Model'
+	'./StatefulObject'
 ], function (
 	declare,
-	
-	Model
 ) {
-	var LinkedObject = declare ([], {
+	var LinkedObject = declare ([StatefulObject], {
 		_source: null,
+		_watchHandle: null,
 		
 		constructor: function (source) {
-			
+			this._source = source;
+			this._watchHandle = this._source.watch (lang.hitch (this, function (name, oldValue, newValue) {
+				
+			}));
 		},
 		
-		get: function () {
+		/**
+		 * Returns a value from this object if this object has a value, otherwise delegates to
+		 * the source object.
+		 */
+		get: function (name) {
+			if (name in this._content[name]) {
+				return this._content[name];
+			}
 			
-		},
-		
-		set: function () {
+			var value = this._source.get (name);
 			
-		},
-		
-		watch: function () {
+			if (typeof value == 'object' && value.watch) {
+				value = linkedCopy (value);
+				this._content[name] value;
+			}
 			
+			return value;
 		},
 		
 		extract: function () {
-			
+			return lang.mixin (lang.mixin ({ }, this._source.extract ()), this.inherited (arguments));
 		}
 	});
 	
