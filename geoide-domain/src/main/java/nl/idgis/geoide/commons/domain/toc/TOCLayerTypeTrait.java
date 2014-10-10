4db2b7cf5baafc562a78327706f9e78ddc1348f7
Linda <Linda@OudeIJssel>
2014-10-10 12:04:31 +0200

    added LayerType and ServiceType Traits for TOC

--- geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/toc/TOCLayerTypeTrait.java
+++ geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/toc/TOCLayerTypeTrait.java
@@ -1,5 +1,11 @@
 package nl.idgis.geoide.commons.domain.toc;
 
-public interface TOCLayerTypeTrait {
+import java.util.List;
 
+import nl.idgis.geoide.commons.domain.traits.Traits;
+import nl.idgis.geoide.commons.layer.LayerType;
+import nl.idgis.geoide.commons.layer.LayerTypeTrait;
+
+public interface TOCLayerTypeTrait extends LayerTypeTrait {
+	public abstract List<Traits<TOCItem>> getTOC(LayerType type);
 }
