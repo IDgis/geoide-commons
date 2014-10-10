5f232885552949aae6eed0c788854ede37bfc496
Linda <Linda@OudeIJssel>
2014-10-10 17:34:42 +0200

    moved the layer and service traits

--- geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/toc/LegendGraphicServiceTypeTrait.java
+++
@@ -1,9 +0,0 @@
-package nl.idgis.geoide.commons.domain.toc;
-
-import nl.idgis.geoide.service.ServiceTypeTrait;
-
-public interface LegendGraphicServiceTypeTrait extends ServiceTypeTrait {
-	
-		public String getLegendGraphic();
-
-}
