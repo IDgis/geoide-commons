1196b449ba916aa29c8281d055cf4bb70541489f
Linda <Linda@OudeIJssel>
2014-10-17 17:33:45 +0200

    eventhandler on layer for settingCheckBoxes

--- geoide-viewer/modules/core/app/assets/core/js/LayerVisibleBehaviour.js
+++ geoide-viewer/modules/core/app/assets/core/js/LayerVisibleBehaviour.js
@@ -4,6 +4,7 @@ define ([
    
    'dojo/dom-attr',
    'dojo/on',
+   'dojo/query',
    
    'dojo/when',
    'dojo/Deferred',
@@ -17,6 +18,7 @@ function(
 	
 	domAttr,
 	on,
+	query,
 	
 	when,
 	Deferred,
@@ -31,30 +33,71 @@ function(
 			
 			var promise = this.inherited(arguments),
 				deferred = new Deferred();
+			
 			promise.then(lang.hitch(this, function() {
+				
 				var thisObject = this;
+				
+				when (this.map, function(map) {
+					var layers = map.get('layerList');
+					layers.forEach (function (layer) {
+						
+						layer.get ('state').watch ('visible', function(property, oldValue, newValue) { 
+								thisObject.setCheckBox(layer, newValue);
+						});
+						
+					});
+				});
+				
 				on (this.node, '.toc-checkbox:click', function (e) {
 					e.preventDefault();
 					e.stopPropagation();
-					
-					var parent = this;
-					while (parent && !parent.dataset.layerId) {
-						parent = parent.parentNode;
-					}
-					var layerId = parent.dataset.layerId;
-					when (thisObject.map, function(map) {
-						var layer = map.get ('layerDictionary').get (layerId);
-						var layerVisible = layer.get ('state').get ('visible');
-						layer.get ('state').set ('visible', !layerVisible);
-					});
-					
+					thisObject.switchVisibility(this); 
 				});
+				
 				deferred.resolve();
 			}));
 			
+			
+			
 			return deferred;
-		}
+			
+		},
+	
+		switchVisibility: function (checkboxNode) {
+			
+			var layerId = this.getLayerId(checkboxNode);
+			
+			when (this.map, function(map) {
+				var layer = map.get ('layerDictionary').get (layerId);
+				var layerVisible = layer.get ('state').get ('visible');
+				layer.get ('state').set ('visible', !layerVisible);
+			});
+			
+		},
 		
+		getLayerId: function (node) {
+			var parent = node;
+			
+			while (parent && !parent.dataset.layerId) {
+				parent = parent.parentNode;
+			}
+			var layerId = parent.dataset.layerId;
+			return layerId;
+			
+		},
+
+		setCheckBox: function(layer, visible) {
+			window.setTimeout (function () {
+				query ('*[data-layer-id="' + layer.get ('id') + '"]', this.node).forEach (function (layerNode) {
+					query ('> label .toc-checkbox', layerNode).forEach(function (checkBoxNode) {
+						console.log (checkBoxNode);
+						checkBoxNode.checked = visible;
+					});
+				});
+			}, 0);
+		}
+	
 		
 	});
 	
