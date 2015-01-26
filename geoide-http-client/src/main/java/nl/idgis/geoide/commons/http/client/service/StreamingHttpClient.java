9bce7ef9bd7119f61c99435be0c0f017eb9b51e7
Erik Orbons <erik.orbons@idgis.nl>
2015-01-26 14:28:54 +0100

    Added experimental StreamingHttpClient: a reactive pull implementation of HttpClient. Not yet functional.

---
+++ geoide-http-client/src/main/java/nl/idgis/geoide/commons/http/client/service/StreamingHttpClient.java
@@ -0,0 +1,218 @@
+package nl.idgis.geoide.commons.http.client.service;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import nl.idgis.geoide.commons.http.client.HttpClient;
+import nl.idgis.geoide.commons.http.client.HttpRequest;
+import nl.idgis.geoide.commons.http.client.HttpRequestBuilder;
+import nl.idgis.geoide.commons.http.client.HttpResponse;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.nio.ContentDecoder;
+import org.apache.http.nio.ContentEncoder;
+import org.apache.http.nio.IOControl;
+import org.apache.http.nio.protocol.HttpAsyncRequestProducer;
+import org.apache.http.nio.protocol.HttpAsyncResponseConsumer;
+import org.apache.http.protocol.HttpContext;
+
+import play.libs.F.Promise;
+
+public class StreamingHttpClient implements HttpClient, Closeable {
+
+	private final CloseableHttpAsyncClient client;
+	
+	public StreamingHttpClient (final long connectionTimeoutMillis) {
+		this.client = HttpAsyncClientBuilder
+			.create ()
+			.build ();
+	}
+	
+	@Override
+	public void close () throws IOException {
+		client.close ();
+	}
+	
+	@Override
+	public HttpRequestBuilder request () {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpRequestBuilder url (final String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Promise<HttpResponse> request (final HttpRequest request) {
+		final HttpAsyncRequestProducer requestProducer = new HttpAsyncRequestProducer () {
+			@Override
+			public void close() throws IOException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void resetRequest() throws IOException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void requestCompleted(HttpContext arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void produceContent(ContentEncoder arg0, IOControl arg1)
+					throws IOException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean isRepeatable() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public HttpHost getTarget() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public org.apache.http.HttpRequest generateRequest() throws IOException,
+					HttpException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void failed(Exception arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		final HttpAsyncResponseConsumer<HttpResponse> responseConsumer = new HttpAsyncResponseConsumer<HttpResponse> () {
+
+			@Override
+			public void close() throws IOException {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public boolean cancel() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void consumeContent(ContentDecoder arg0, IOControl arg1)
+					throws IOException {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void failed(Exception arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public Exception getException() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public HttpResponse getResult() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public boolean isDone() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void responseCompleted(HttpContext arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void responseReceived(org.apache.http.HttpResponse arg0)
+					throws IOException, HttpException {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		final FutureCallback<HttpResponse> callback = new FutureCallback<HttpResponse> () {
+			@Override
+			public void cancelled() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void completed(HttpResponse arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void failed(Exception arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		client.execute (requestProducer, responseConsumer, callback);
+		
+		/*
+		BoundRequestBuilder builder = client
+			.prepareConnect (request.getUrl ())
+			.setMethod (request.getMethod ().name ())
+			.setFollowRedirects (request.isFollowRedirects ());
+		
+		for (final Map.Entry<String, List<String>> entry: request.getParameters ().entrySet ()) {
+			for (final String value: entry.getValue ()) {
+				builder = builder.addQueryParameter (entry.getKey (), value);
+			}
+		}
+		
+		for (final Map.Entry<String, List<String>> entry: request.getHeaders ().entrySet ()) {
+			for (final String value: entry.getValue ()) {
+				builder = builder.addHeader (entry.getKey (), value);
+			}
+		}
+		
+		final Publisher<ByteString> body = request.getBody ();
+		if (body != null) {
+			builder.setBody (dataWriter)
+			builder.setBody (createBodyGenerator (body));
+		}
+
+		try {
+			return execute (request, builder);
+		} catch (IOException e) {
+			return Promise.throwing (e);
+		}
+		*/
+		
+		return null;
+	}
+}
