0d4528e1a0d36085a5d01bf025b6814fb7173cfc
Erik Orbons <erikorbons@users.noreply.github.com>
2015-03-16 13:58:03 +0100

    Moved requirejs implementation to JavaScript code.

--- geoide-print/src/main/java/nl/idgis/geoide/commons/report/layout/less/LessCompiler.java
+++ geoide-print/src/main/java/nl/idgis/geoide/commons/report/layout/less/LessCompiler.java
@@ -13,7 +13,6 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import javax.xml.bind.DatatypeConverter;
 
 public class LessCompiler {
 
@@ -23,7 +22,7 @@ public class LessCompiler {
 	private final ScriptEngineManager scriptEngineManager;
 	private final ScriptEngine scriptEngine;
 	
-	private final Map<String, Object> requireJsCache = new HashMap<> ();
+	public final Map<String, Object> requireJsCache = new HashMap<> ();
 	
 	public LessCompiler (final String lessVersion) {
 		// Create a JavaScript engine:
@@ -36,77 +35,30 @@ public class LessCompiler {
 		// Compile the main script:
 		try  {
 			scriptEngine.put ("req", this);
-			scriptEngine.put ("env", new Environment ());
-			scriptEngine.put ("fs", new Filesystem ());
 			
 			scriptEngine.getBindings (ScriptContext.ENGINE_SCOPE).put (ScriptEngine.FILENAME, "less-compiler.js");
 			scriptEngine.eval (new InputStreamReader (LessCompiler.class.getClassLoader ().getResourceAsStream ("nl/idgis/geoide/commons/report/layout/less/less-compiler.js")));
-			
-			// scriptEngine.eval ("print (new less.Parser ().parser ('a { }'));");
-			scriptEngine.eval ("less.render (\"\\na {\\n }\\n\", { processImports: false }, function (a, b) { print (a); print (b); });");
-			//scriptEngine.eval ("var parser = new less.Parser ({ processImports: false }, new less.ImportManager ()); parser.parse ('a { }', function (e, r) { print (r); });");
 		} catch (ScriptException e) {
 			throw new RuntimeException (e);
 		}
 	}
 
-	private String combine (String basePath, String path) {
-		while (true) {
-			if (path.startsWith ("./")) {
-				path = path.substring (2);
-			} else if (path.startsWith ("../")) {
-				final int n = basePath.lastIndexOf ('/');
-				if (n >= 0) {
-					basePath = basePath.substring (0, n);
-				}
-				path = path.substring (3);
-			} else {
-				break;
-			}
-		}
-		
-		return basePath + "/" + path;
-	}
-	
-	public Object require (final String path) {
-		return require (path, null);
-	}
-	
-	public Object require (final String path, final String basePath) {
-		final String absolutePath;
-		
-		if (basePath != null && path.startsWith (".")) {
-			final int n = basePath.lastIndexOf ('/');
-			
-			if (n >= 0) {
-				absolutePath = combine (basePath.substring (0, n), path);
-			} else {
-				absolutePath = combine ("", path);
-			}
-		} else {
-			absolutePath = path;
-		}
-		
-		if (requireJsCache.containsKey (absolutePath)) {
-			return requireJsCache.get (absolutePath);
-		}
-		
+	public String[] load (final String path) {
 		final String finalPath;
-		
 		final InputStream is;
-		final InputStream unmodified = tryOpen (absolutePath);
+		final InputStream unmodified = tryOpen (path);
 		if (unmodified == null) {
-			final String indexPath = absolutePath + (absolutePath.endsWith ("/") ? "" : "/") + "index";
+			final String indexPath = path + (path.endsWith ("/") ? "" : "/") + "index";
 			final InputStream index = tryOpen (indexPath);
+			finalPath = indexPath;
 			if (index != null) {
 				is = index;
-				finalPath = indexPath;
 			} else {
 				throw new RuntimeException ("Path not found: " + path);
 			}
 		} else {
 			is = unmodified;
-			finalPath = absolutePath;
+			finalPath = path;
 		}
 		
 		try (final InputStream inputStream = is) {
@@ -127,34 +79,14 @@ public class LessCompiler {
 					}
 				}
 		
-				scriptEngine.getBindings (ScriptContext.ENGINE_SCOPE).put (ScriptEngine.FILENAME, finalPath + ".js");
-				
-				final Object value = scriptEngine.eval ("(function () { var module = { }; var require = function (a) { return req.require (a, '" + finalPath + "'); }; " + content + "\nreturn module.exports; }) ();");
-
-				requireJsCache.put (absolutePath, value);
-				
-				return value;
+				return new String[] { finalPath, "//# sourceUrl=" + finalPath + ".js\n" + content.toString () };
 			}
 		} catch (IOException e) {
-			throw new RuntimeException (e);
-		} catch (ScriptException e) {
 			throw new RuntimeException (e);
 		}
 	}
 	
 	private InputStream tryOpen (final String path) {
 		return LessCompiler.class.getClassLoader ().getResourceAsStream (lessPath + path + ".js");
-	}
-	
-	// Must implement the api specified in environment-api.js:
-	public static class Environment {
-		public String encodeBase64 (final String input) {
-			return DatatypeConverter.printBase64Binary (input.getBytes ());
-		}
-	}
-	
-	// Must implement the api specified in file-manager-api.js:
-	public static class Filesystem {
-		
 	}
 }
--- geoide-print/src/main/resources/nl/idgis/geoide/commons/report/layout/less/less-compiler.js
+++ geoide-print/src/main/resources/nl/idgis/geoide/commons/report/layout/less/less-compiler.js
@@ -2,8 +2,67 @@
 var window = this;
 
 // Require js:
-function require (a) { 
-	return req.require (a); 
+var requireJsCache = { };
+
+function combine (basePath, path) {
+	while (true) {
+		if (path.substring (0, 2) == "./") {
+			path = path.substring (2);
+		} else if (path.substring (0, 3) == "../") {
+			var n = basePath.lastIndexOf ('/');
+			if (n >= 0) {
+				basePath = basePath.substring (0, n);
+			}
+			path = path.substring (3);
+		} else {
+			break;
+		}
+	}
+	
+	return basePath + "/" + path;
+}
+
+function doRequire (dep, base) {
+	// Make the path absolute:
+	if (base && dep.substring (0, 1) == '.') {
+		var n = base.lastIndexOf ('/');
+		
+		if (n >= 0) {
+			dep = combine (base.substring (0, n), dep);
+		} else {
+			dep = combine ('', dep);
+		}
+	}
+	
+	// Check the cache:
+	if (dep in requireJsCache) {
+		return requireJsCache[dep];
+	}
+	
+	// Load the dependency:
+	var result = req.load (dep),
+		finalPath = result[0],
+		content = result[1];
+	
+	var exports = (function () {
+		var require = function (dep) {
+			return doRequire (dep, finalPath);
+		};
+		
+		var module = { };
+		
+		eval (content);
+
+		return module.exports;
+	}) ();
+	
+	requireJsCache[dep] = exports;
+	
+	return exports;
+}
+
+function require (dep) {
+	return doRequire (dep);
 }
 
 // Mocked Promise implementation:
@@ -18,4 +77,54 @@ Promise.prototype.reject = function (a) {
 	print ('Reject: ', a); 
 };			
 
+// Utility for printing errors:
+function printError (error) {
+	print (error.filename + ":" + error.line + ":" + error.column + ": " + error.message + " (" + error.type + "): ");
+	print (error.stack);
+}
+
+// Require the less compiler:
+require ('less/tree/element');
+
+var old = req.requireJsCache['less/tree/element'];
+req.requireJsCache['less/tree/element'] = function (c, value) {
+	print ("Element: " + c + ", " + value + ", " + (c instanceof require ('less/tree/combinator')));
+	for (var i in c) {
+		print (i + ": " + c[i]);
+	}
+	
+	old.apply (this, arguments);
+	
+	print ("End element");
+};
+
 var less = require ('less')(/*null, [fs]*/);
+
+// Entrypoint to be called from the Java code:
+function lessCompile (input) {
+	var compileError, compileResult;
+	
+	less.render (
+		input, 
+		{ 
+			processImports: false 
+		}, 
+		function (error, result) {
+			if (error) {
+				printError (error);
+				compileError = error;
+				return;
+			}
+
+			compileResult = result;
+		}
+	);
+	
+	if (compileError) {
+		throw compileError;
+	}
+	
+	return compileResult.css;
+}
+
+print (lessCompile (".a { display: block; }"))
