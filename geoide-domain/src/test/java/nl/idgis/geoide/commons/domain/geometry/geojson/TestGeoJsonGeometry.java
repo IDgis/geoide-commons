6e364c827bdaea80ee00bd43649e8d519ed5dc27
Erik Orbons <erikorbons@users.noreply.github.com>
2015-04-28 16:45:19 +0200

    Organized imports.

--- geoide-domain/src/test/java/nl/idgis/geoide/commons/domain/geometry/geojson/TestGeoJsonGeometry.java
+++ geoide-domain/src/test/java/nl/idgis/geoide/commons/domain/geometry/geojson/TestGeoJsonGeometry.java
@@ -2,7 +2,6 @@ package nl.idgis.geoide.commons.domain.geometry.geojson;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
