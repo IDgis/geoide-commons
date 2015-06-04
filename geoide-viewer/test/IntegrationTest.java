38c9104fea7ab308700577b6ff0e34784d964981
Erik Orbons <erik.orbons@idgis.nl>
2015-06-04 15:49:52 +0200

    Removed integration test.

--- geoide-viewer/test/IntegrationTest.java
+++ geoide-viewer/test/IntegrationTest.java
@@ -17,12 +17,14 @@ public class IntegrationTest {
      */
     @Test
     public void test() {
+    	/*
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333/map/afdelingen");
                 // assertThat(browser.pageSource()).contains("id=\"viewer-container\"");
             }
         });
+        */
     }
 
 }
