24fc176da8b6c94580fbd5d3ef91d4ec8702b008
LindaVels <linda.vels@idgis.nl>
2015-09-16 09:21:53 +0200

    bbox in FeatureQuery

--- geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/FeatureQuery.java
+++ geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/FeatureQuery.java
@@ -1,7 +1,23 @@
 package nl.idgis.geoide.commons.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.Optional;
+
+import nl.idgis.geoide.commons.domain.geometry.Envelope;
 
 public final class FeatureQuery implements Serializable {
 	private static final long serialVersionUID = -272100533712128453L;
+	
+	private final Envelope bbox;
+	
+	public FeatureQuery (final Optional<Envelope> bbox) {
+		Objects.requireNonNull (bbox, "bbox cannot be null");
+		
+		this.bbox = bbox.isPresent () ? bbox.get () : null;
+	}
+
+	public Optional<Envelope> getBbox() {
+		return Optional.ofNullable (bbox);
+	}
 }
