a326b076c17027d8d132f204f7118bf0b069de60
LindaVels <linda.vels@idgis.nl>
2015-10-05 15:23:59 +0200

    ServiceRequest implements Serializable

--- geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/ServiceRequest.java
+++ geoide-domain/src/main/java/nl/idgis/geoide/commons/domain/ServiceRequest.java
@@ -2,11 +2,13 @@ package nl.idgis.geoide.commons.domain;
 
 import nl.idgis.geoide.util.Assert;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public final class ServiceRequest {
+public final class ServiceRequest implements Serializable {
 	
 	private static final long serialVersionUID = 2821223114900949315L;
 	
@@ -28,8 +30,8 @@ public final class ServiceRequest {
 	public JsonNode serialize () {
 		final ObjectNode obj = JsonFactory.mapper ().createObjectNode ();
 
-		obj.put ("id", getId ());
-		obj.put ("serviceId", getService ().getId ());
+		obj.put ("id", id);
+		obj.put ("serviceId", JsonFactory.asJson (getService ().getId ()));
 		obj.put ("serviceIdentification", JsonFactory.asJson (getService ().getIdentification ()));
 		if (getParameters () != null) {
 			obj.put ("parameters", JsonFactory.asJson (getParameters ()));
