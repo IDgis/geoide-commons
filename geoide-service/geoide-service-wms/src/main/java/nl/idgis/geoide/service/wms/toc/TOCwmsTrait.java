c53ac7a4a6891271b9fe705489595dd51814e6b0
LindaVels <linda.vels@idgis.nl>
2016-10-10 16:23:08 +0200

    Do not encode the "http://" and the "/"in the legend graphic urls

--- geoide-service/geoide-service-wms/src/main/java/nl/idgis/geoide/service/wms/toc/TOCwmsTrait.java
+++ geoide-service/geoide-service-wms/src/main/java/nl/idgis/geoide/service/wms/toc/TOCwmsTrait.java
@@ -25,19 +25,31 @@ public class TOCwmsTrait implements TOCServiceTypeTrait{
 	public List<Traits<TOCItem>>getTOC(final Traits<ServiceType> serviceType, final ServiceLayer serviceLayer) {
 		
 		final List<Traits<TOCItem>> tocItems = new ArrayList<>();
-				
-		try {
-			tocItems.add(Traits.create (
-				TOCItem
-					.builder ()
-					.setLabel (serviceLayer.getLabel ())
-					.setSymbol (new Symbol (serviceLayer.getId(), URLEncoder.encode(serviceLayer.getLegendGraphicUrl(),"UTF8")))
-					.setIsGroup(false)
-					.build ()
-			));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+		String [] urlParts = serviceLayer.getLegendGraphicUrl().split("/");
+		String legendGraphicUrl = "";
+		if (urlParts.length > 0) {
+			legendGraphicUrl = urlParts[0] + "//";
 		}
+
+		for (int n = 1; n < urlParts.length; n++) {
+			try {
+				legendGraphicUrl += URLEncoder.encode(urlParts[n],"UTF8");
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			if (n + 1 != urlParts.length){
+				legendGraphicUrl += "/";
+			}
+		}
+		tocItems.add(Traits.create (
+			TOCItem
+				.builder ()
+				.setLabel (serviceLayer.getLabel ())
+				.setSymbol (new Symbol (serviceLayer.getId(), legendGraphicUrl))
+				.setIsGroup(false)
+				.build ()
+		));
+		
 			
 		return Collections.unmodifiableList(tocItems);
 	}
