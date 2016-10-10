2d2b588fe4ddac25a219c34ffef06b1513a20a01
LindaVels <linda.vels@idgis.nl>
2016-10-10 15:15:31 +0200

    Url encode the legendGraphic Url

--- geoide-service/geoide-service-wms/src/main/java/nl/idgis/geoide/service/wms/toc/TOCwmsTrait.java
+++ geoide-service/geoide-service-wms/src/main/java/nl/idgis/geoide/service/wms/toc/TOCwmsTrait.java
@@ -1,5 +1,7 @@
 package nl.idgis.geoide.service.wms.toc;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,16 +25,19 @@ public class TOCwmsTrait implements TOCServiceTypeTrait{
 	public List<Traits<TOCItem>>getTOC(final Traits<ServiceType> serviceType, final ServiceLayer serviceLayer) {
 		
 		final List<Traits<TOCItem>> tocItems = new ArrayList<>();
-		
-		
-		tocItems.add(Traits.create (
-			TOCItem
-				.builder ()
-				.setLabel (serviceLayer.getLabel ())
-				.setSymbol (new Symbol (serviceLayer.getId(), serviceLayer.getLegendGraphicUrl()))
-				.setIsGroup(false)
-				.build ()
-		));
+				
+		try {
+			tocItems.add(Traits.create (
+				TOCItem
+					.builder ()
+					.setLabel (serviceLayer.getLabel ())
+					.setSymbol (new Symbol (serviceLayer.getId(), URLEncoder.encode(serviceLayer.getLegendGraphicUrl(),"UTF8")))
+					.setIsGroup(false)
+					.build ()
+			));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 			
 		return Collections.unmodifiableList(tocItems);
 	}
